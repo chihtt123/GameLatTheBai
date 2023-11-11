2e877087dcf1e9d781d07e93bcae47a15b7f365e
chihtt123 <chihtt1234@gmail.com>
2023-11-11 22:57:56 +0700

    init project

---
+++ Assets/_Data/Card/CardSystem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSystem : ChiMonoBehaviour
+{
+    List<Node> nodes;
+    int width = 4;
+    int height = 4; 
+}
---
+++ Assets/_Data/Card/Node.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public int x = 0;
+    public int y = 0;
+    public bool occupied = false;
+}
