7fec633d2dcfcc65790e43664781daf56dc22a44
chihtt123 <chihtt1234@gmail.com>
2023-11-23 00:40:28 +0700

    D3

--- Assets/_Data/Block/BlockItem.cs
+++ Assets/_Data/Block/BlockItem.cs
@@ -9,7 +9,7 @@ public class BlockItem : MonoBehaviour
     public int Value => value;
     [SerializeField] private bool isFlipped = false;
     [SerializeField] private BlockManager blockManager ;
-
+    
 
     protected virtual void Start()
     {
@@ -20,20 +20,39 @@ public class BlockItem : MonoBehaviour
     public void FlipCard()
     {
         if (!isFlipped && blockManager.CanFlip()) 
-        {
-
+        {     
             isFlipped = true;
-
-            blockManager.AddFlippedCard(this); 
+            blockManager.AddFlippedCard(this);
+            this.imageTransfer();
         }
         Debug.Log(isFlipped +" "+  blockManager.CanFlip());
     }
 
-    public void UnflipCard()
+    protected virtual void imageTransfer()
     {
 
+        if (isFlipped)
+        {
+            Image buttonImage = this.GetComponent<Image>();
+  
+            int indexImage = this.value;
+            buttonImage.sprite = blockManager.sprites[indexImage];    
+        }
+        else
+        {
+            UnflipCard();
+        }
+       
+        
+    }
+
+    public void UnflipCard()
+    {     
         isFlipped = false;
-      
+       
+        Image buttonImage = this.GetComponent<Image>();
+        buttonImage.sprite = blockManager.sprites[blockManager.sprites.Count-1];
+
     }
 
 
--- Assets/_Data/Block/BlockManager.cs
+++ Assets/_Data/Block/BlockManager.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlockManager : MonoBehaviour
 {
     [SerializeField] protected Transform blockPrefab;
 
     [SerializeField] protected Transform GameUI;
+
+    [SerializeField] private int score;
+
+    [SerializeField] private int remainingTurn;
+    [SerializeField] private int correctPair;
 
     public int rows = 3;
     public int cols = 4;
@@ -16,6 +22,10 @@ public class BlockManager : MonoBehaviour
 
     private List<int> cardValues = new List<int>();
     [SerializeField] private List<BlockItem> flippedBLock = new List<BlockItem>();
+    [SerializeField] public List<Sprite> sprites = new List<Sprite>();
+
+    public int Score { get => score; set => score = value; }
+
 
 
     protected virtual void Start()
@@ -25,6 +35,10 @@ public class BlockManager : MonoBehaviour
 
     protected virtual void InitializeBlock()
     {
+        this.score = 0;
+        this.remainingTurn = 20;
+        this.correctPair = 0;
+
         blockPrefab.gameObject.SetActive(false);
         List<int> values = new List<int>();
         for (int i = 0; i < (rows * cols) / 2; i++)
@@ -42,8 +56,7 @@ public class BlockManager : MonoBehaviour
                 Transform card = Instantiate(blockPrefab, new Vector3(j, -i, 0), Quaternion.identity);
                 card.GetComponent<BlockItem>().SetValue(values[i * cols + j]);
                 card.gameObject.SetActive(true);
-                card.SetParent(this.GameUI);
-               
+                card.SetParent(this.GameUI);     
             }
         }
         return; 
@@ -74,6 +87,7 @@ public class BlockManager : MonoBehaviour
         Debug.Log("Game");
         if (flippedBLock.Count == 2)
         {
+            remainingTurn--;
             StartCoroutine(CheckMatch());
         }
     }
@@ -81,15 +95,16 @@ public class BlockManager : MonoBehaviour
     IEnumerator CheckMatch()
     {
         canFlip = false;
-
         yield return new WaitForSeconds(1.5f); 
         if (flippedBLock[0].Value == flippedBLock[1].Value)
         {
-           
+            score+= 5;      
+            flippedBLock[0].gameObject.SetActive(false);
+            flippedBLock[1].gameObject.SetActive(false);
         }
         else
         {
-            // No match
+            score--;
             flippedBLock[0].UnflipCard();
             flippedBLock[1].UnflipCard();
         }
